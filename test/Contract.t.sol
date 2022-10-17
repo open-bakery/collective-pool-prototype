a921a4fc90907bd90cc30cb2fa26b34b12dc62e7
0xnavigator <0xnavigator@proton.me>
2022-10-17 09:22:20 +0200

    test for DepositRatioCalculator

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,12 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.5.0 <0.8.0;
 
-import "forge-std/Test.sol";
+import './LocalVars.t.sol';
+import '../src/DepositRatioCalculator.sol';
+import '../src/logs/Logs.sol';
+import 'forge-std/Test.sol';
 
-contract ContractTest is Test {
-    function setUp() public {}
+contract ContractTest is Test, Logs, LocalVars {
+  DepositRatioCalculator drc = new DepositRatioCalculator();
 
-    function testExample() public {
-        assertTrue(true);
-    }
+  function setUp() public {}
+
+  function testDepositRatioCalculator() public {
+    uint256 amountA = 0.2178 ether;
+    uint256 amountB = 57_800000;
+
+    (uint256 amount0Ratioed, uint256 amount1Ratioed) = drc.calculateDepositRatio(
+      MAIN_WETH,
+      MAIN_USDC,
+      500,
+      amountA,
+      amountB,
+      1220_000000,
+      1419_000000
+    );
+
+    logr(
+      'testDepositRatioCalculator()',
+      ['amount0Ratioed', 'amount1Ratioed', '0', '0', '0', '0'],
+      [uint256(amount0Ratioed), amount1Ratioed, 0, 0, 0, 0]
+    );
+  }
 }
