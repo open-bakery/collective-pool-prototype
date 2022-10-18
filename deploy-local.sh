4d6bdf3b878e8e772efab7194fecfed5149812c2
Jiri Blaha <blaha@wedecom.cz>
2022-10-18 21:59:59 +0200

    Output from deploy script to json

--- deploy-local.sh
+++ deploy-local.sh
@@ -3,4 +3,5 @@ echo "getting env vars from $BASEDIR/.env"
 . "$BASEDIR/.env"
 
 echo "deploying locally with key: $DEV_PRIVATE_KEY"
-RUST_BACKTRACE=full forge script script/DevDeploy.s.sol:Deploy --fork-url http://127.0.0.1:8545 --private-key $DEV_PRIVATE_KEY --broadcast -vvv
+rm "$BASEDIR/$DEPLOY_OUT"
+RUST_BACKTRACE=full forge script script/DevDeploy.s.sol:Deploy --fork-url http://127.0.0.1:8545 --private-key $DEV_PRIVATE_KEY --broadcast
--- script/DevDeploy.s.sol
+++ script/DevDeploy.s.sol
@@ -11,12 +11,38 @@ import '../src/RangePool.sol';
 contract Deploy is Script {
   address WETH = vm.envAddress('WETH');
   address USDC = vm.envAddress('USDC');
+  string DEPLOY_OUT = vm.envString('DEPLOY_OUT');
 
   uint24 FEE0_05 = 500;
   uint24 FEE0_30 = 3000;
   uint24 FEE1_00 = 10000;
 
   function setUp() public {}
+
+  function concat(string memory a, string memory b) public pure returns (string memory) {
+    return string(abi.encodePacked(a, b));
+  }
+
+  function quote(string memory str) public pure returns (string memory) {
+    return concat('"', concat(str, '"'));
+  }
+
+  function jsonProp(string memory prop, string memory val) public pure returns (string memory) {
+    return concat(quote(prop), concat(': ', quote(val)));
+  }
+
+  function outputStart() public {
+    vm.writeLine(DEPLOY_OUT, '{');
+  }
+
+  function outputEnd() public {
+    vm.writeLine(DEPLOY_OUT, '  "finalProp": "Need this so that the last line has no comma"');
+    vm.writeLine(DEPLOY_OUT, '}');
+  }
+
+  function outputProp(string memory prop, string memory val) public {
+    vm.writeLine(DEPLOY_OUT, concat('  ', concat(jsonProp(prop, val), ',')));
+  }
 
   function usdcAmount(uint256 amount) private pure returns (uint256) {
     return amount * 10**6;
@@ -32,12 +58,15 @@ contract Deploy is Script {
 
   function run() external {
     vm.startBroadcast();
+    outputStart();
 
     RangePool pool1 = new RangePool(WETH, USDC, FEE0_30, usdcAmount(1000), usdcAmount(2000));
-    console.log('Pool1 deployed: ', address(pool1));
-    RangePool pool2 = new RangePool(WETH, USDC, FEE0_30, usdcAmount(500), usdcAmount(4000));
-    console.log('Pool2 deployed: ', address(pool2));
+    outputProp('Pool1', vm.toString(address(pool1)));
 
+    RangePool pool2 = new RangePool(WETH, USDC, FEE0_30, usdcAmount(500), usdcAmount(4000));
+    outputProp('Pool2', vm.toString(address(pool2)));
+
+    outputEnd();
     vm.stopBroadcast();
   }
 }
